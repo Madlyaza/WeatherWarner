45f48ff689046636d7af4a4d9901bf8781605bdb
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2021-08-02 16:09:30 +0200

    Update

--- WeatherWarner2.0/src/com/NHLStenden/ApiCaller.java
+++ WeatherWarner2.0/src/com/NHLStenden/ApiCaller.java
@@ -1,5 +1,7 @@
 package com.NHLStenden;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -31,10 +33,10 @@ public class ApiCaller
         return(response.body());
     }
 
-    public String getForecastThreeHours(String location)
+    public void getForecastThreeHours(String location)
     {
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://community-open-weather-map.p.rapidapi.com/forecast?q="+ location +"%2Cnl&units=metric&mode=json&lang=en&cnt=2"))
+                .uri(URI.create("https://community-open-weather-map.p.rapidapi.com/forecast?q="+ location +"%2Cnl&units=metric&mode=xml&lang=en&cnt=2"))
                 .header("x-rapidapi-key", "e0f745ce19mshde257cf3b9d08bcp15b392jsnae76ad93ec4d")
                 .header("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -50,6 +52,30 @@ public class ApiCaller
         {
             e.printStackTrace();
         }
-        return(response.body());
+
+        try {
+            File myObj = new File("XmlParsing.xml");
+            if(myObj.createNewFile())
+            {
+                System.out.println("File created: " + myObj.getName());
+            }
+            else
+            {
+                System.out.println("file already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter("XmlParsing.xml");
+            myWriter.write(response.body());
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
