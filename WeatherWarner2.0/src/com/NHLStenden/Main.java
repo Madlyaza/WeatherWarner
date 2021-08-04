da0f8b38007712a2497c6b0779ee8016b55146d3
Casper van den Berg <casper.van.den.berg@student.nhlstenden.com>
2021-08-04 09:40:18 +0200

    Cleanup main.java

--- WeatherWarner2.0/src/com/NHLStenden/Main.java
+++ WeatherWarner2.0/src/com/NHLStenden/Main.java
@@ -16,57 +16,10 @@ public class Main
         Sound sound = new Sound();
         ApiCaller api = new ApiCaller();
 
-        Thread precipitationAlarm = new Thread(() ->
-        {
-            while(true)
-            {
-                try
-                {
-                    api.getForecastThreeHours(user.getLocation());
-                    XmlPrecipitationAlarm xmlPrecipitationAlarm = new XmlPrecipitationAlarm();
-                    if (xmlPrecipitationAlarm.parseXML())
-                    {
-                        sound.playSound();
-                    }
-                    Thread.sleep(1000 * 60 * 60 * 3);
-                }
-                catch (InterruptedException | ParserConfigurationException | IOException | SAXException ie)
-                {
-                    System.out.println("The precipitation timer has stopped. Please restart the application to make sure the timer is working again.");
-                    ie.printStackTrace();
-                }
-            }
-        });
-        precipitationAlarm.start();
+        PrecipitationAlarm precipitationAlarm = new PrecipitationAlarm();
+        precipitationAlarm.start(api, user, sound);
 
-        Thread frostAndHeatAlarm = new Thread(() ->
-        {
-            while(true)
-            {
-                try
-                {
-                    api.getForecastOneDay(user.getLocation());
-                    XmlHeatAlarm xmlHeatAlarm = new XmlHeatAlarm();
-                    if (xmlHeatAlarm.checkTemperature(xmlHeatAlarm.parseXML()))
-                    {
-                        sound.playSound();
-                    }
-
-                    XmlFrostAlarm xmlFrostAlarm = new XmlFrostAlarm();
-                    if (xmlFrostAlarm.checkTemperature(xmlFrostAlarm.parseXML()))
-                    {
-                        sound.playSound();
-                    }
-
-                    Thread.sleep(1000 * 60 * 60 * 24);
-                }
-                catch (InterruptedException | ParserConfigurationException | IOException | SAXException ie)
-                {
-                    System.out.println("The heat and cold timer has stopped working. Please restart the application to make sure the time is working again.");
-                    ie.printStackTrace();
-                }
-            }
-        });
-        frostAndHeatAlarm.start();
+        FrostAndHeatAlarm frostAndHeatAlarm = new FrostAndHeatAlarm();
+        frostAndHeatAlarm.start(api, user, sound);
     }
 }
