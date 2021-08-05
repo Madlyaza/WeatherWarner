8598553cee6dbfabb0fae4bfe13dcb72fc3c5cd2
Casper van den Berg <casper.van.den.berg@student.nhlstenden.com>
2021-08-05 11:47:08 +0200

    Deleted old login and added styling changes

--- WeatherWarner2.0/src/com/NHLStenden/LoginPage.java
+++ WeatherWarner2.0/src/com/NHLStenden/LoginPage.java
@@ -14,18 +14,18 @@ public class LoginPage implements ActionListener
     JPasswordField userPasswordField = new JPasswordField();
     JLabel userNameLabel = new JLabel("Username:");
     JLabel userPasswordLabel = new JLabel("Password:");
-    JLabel messageLabel = new JLabel("Moi hoe is het nou dan?");
+    JLabel messageLabel = new JLabel("");
 
     public LoginPage()
     {
         userNameLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,100,75,25);
+        userPasswordLabel.setBounds(50,150,75,25);
 
-        messageLabel.setBounds(125,250,250,35);
+        messageLabel.setBounds(125,250,350,35);
         messageLabel.setFont(new Font(null,Font.ITALIC,25));
 
-        userNameField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
+        userNameField.setBounds(125,100,250,25);
+        userPasswordField.setBounds(125,150,250,25);
 
         loginButton.setBounds(125,200,100,25);
         loginButton.addActionListener(this);
@@ -41,7 +41,7 @@ public class LoginPage implements ActionListener
         frame.add(loginButton);
         frame.add(resetButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(240,420);
+        frame.setSize(520,420);
         frame.setLayout(null);
         frame.setVisible(true);
     }
@@ -60,16 +60,27 @@ public class LoginPage implements ActionListener
             String userName = userNameField.getText();
             String userPassword = String.valueOf(userPasswordField.getPassword());
 
-            if (userName == "admin")
+            if (userName.equals("admin") && userPassword.equals("moin"))
             {
-                if (userPassword == "moin");
-                {
-                    messageLabel.setForeground(Color.GREEN);
-                    messageLabel.setText("Login successful");
-                    GUI gui = new GUI();
-                }
+                messageLabel.setForeground(Color.GREEN);
+                messageLabel.setText("Login successful");
+                GUI gui = new GUI();
             }
-
+            else if (userName.equals("admin"))
+            {
+                messageLabel.setForeground(Color.RED);
+                messageLabel.setText("Wrong password");
+            }
+            else if (userPassword.equals("moin"))
+            {
+                messageLabel.setForeground(Color.RED);
+                messageLabel.setText("Username not found");
+            }
+            else
+            {
+                messageLabel.setForeground(Color.RED);
+                messageLabel.setText("Invalid Username & Password!");
+            }
         }
     }
 }
