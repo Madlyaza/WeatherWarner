856e9a9dcba84a694f12fbc3cdcc43233fde1824
Robbie Eijkelkamp <robbie.eijkelkamp@student.nhlstenden.com>
2021-08-18 14:01:19 +0200

    update

---
+++ WeatherWarner2.0/test/com/NHLStenden/SoundTest.java
@@ -0,0 +1,15 @@
+package com.NHLStenden;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoundTest
+{
+
+    @Test
+    void playSound()
+    {
+
+    }
+}
